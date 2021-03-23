3233446ff7fe516a53cd77edbabed63690944a26
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-03-23 23:21:43 +0000

    charCount section complete

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   const charCount = function() {
     const count = 140 - this.value.length;
+    if (count < 0) {
+      $('output').css('color', 'red');
+    }
     $('output').text(count);
   };
   //Counts character
