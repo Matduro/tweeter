a9d9de5a77489b78121388ec0d3344807d4e7a3b
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-03-24 20:08:42 +0000

    dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,51 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// Fake data taken from initial-tweets.json
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+// $(document).ready(function() {
+
+const createTweetElement = function() {
+
+};
+
+// Test / driver code (temporary). Eventually will get this from the server.
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+const renderTweets = function() {
+  // leverage createTweetElement function
+  // take an array of tweet objects
+  // loops and appends each one to the #tweets-container
+};
+
+// });
